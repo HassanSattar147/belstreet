d4e79ecc83efa3149f2a7daddeaca21d336fd8b2
unknown <hassansattar147@gmail.com>
2023-10-11 03:34:33 +0500

    finalized homepage

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,6 +1,10 @@
 // import React from 'react'
 import Button from "../Elements/Button";
 import Nav from "../Nav";
+import "../../styles/homepage.css";
+import UnderLine from "../../../public/assets/common/Under line.png";
+import searchPageIcon from "../../../public/assets/common/search-page-icon.svg";
+import evaluatePageIcon from "../../../public/assets/common/evaluate-page-icon.svg";
 
 const Homepage = ({
   setIsSearchPage,
@@ -10,20 +14,54 @@ const Homepage = ({
   return (
     <div>
       <Nav />
-      <Button
-        onClick={() => {
-          setIsSearchPage(true);
-        }}
-        text="Search Page"
-        variant="primary"
-      />
-      <Button
-        onClick={() => {
-          setIsSearchPage(false);
-        }}
-        text="Evaluation Page"
-        variant="secondary"
-      />
+
+      <div className="homepage-content">
+        <div className="homepage-content-header">
+          <h2>
+            Belstreet evaluates the quality of life in all the{" "}
+            <span className="special-text">
+              streets of Brussels
+              <img src={UnderLine} alt="" />
+            </span>
+            .
+          </h2>
+          <p>
+            Are you moving and want to be 100% sure that there will be no
+            surprises? A noisy or poorly communicated street?
+          </p>
+          <p>BelStreet is at your side to help you make the right choice.</p>
+          <p>
+            Evaluate your street with a few questions or consult our database to
+            consult the evaluation of all the streets in Brussels!
+          </p>
+        </div>
+        <div className="page-nav-cards">
+          <div
+            className="page-card"
+            onClick={() => {
+              setIsSearchPage(false);
+            }}
+          >
+            <img src={evaluatePageIcon} alt="" />
+            <p>
+              Evaluate <br />
+              your street
+            </p>
+          </div>
+          <div
+            className="page-card"
+            onClick={() => {
+              setIsSearchPage(true);
+            }}
+          >
+            <img src={searchPageIcon} alt="" />
+            <p>
+              Search <br />
+              the database
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
