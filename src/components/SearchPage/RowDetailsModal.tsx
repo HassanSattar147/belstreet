cb70db37edd959b61283b56c7aa109c98fd8a84a
unknown <hassansattar147@gmail.com>
2023-10-11 19:12:06 +0500

    added PIE chart

--- src/components/SearchPage/RowDetailsModal.tsx
+++ src/components/SearchPage/RowDetailsModal.tsx
@@ -5,28 +5,26 @@ import icon_car from "../../../public/assets/common/icon-car.svg";
 import LabeledRatings from "../Elements/LabeledRatings";
 import Button from "../Elements/Button";
 
-const RowDetailsModal = ({onClose}:{
-  onClose: () => void
-}) => {
+const RowDetailsModal = ({ onClose }: { onClose: () => void }) => {
   const headingTitle = "Grand Place 0";
 
   const emptyFunc = () => {};
 
   const all_data = [
     {
-      selectedCount: 1,
+      selectedCount: 4,
       setSelectedCount: emptyFunc,
       title: "Airplane Noise",
       image: icon_plane,
     },
     {
-      selectedCount: 5,
+      selectedCount: 4,
       setSelectedCount: emptyFunc,
       title: "Noise from Neighbors",
       image: icon_hear,
     },
     {
-      selectedCount: 3,
+      selectedCount: 5,
       setSelectedCount: emptyFunc,
       title: "Traffic noise",
       image: icon_car,
@@ -39,12 +37,31 @@ const RowDetailsModal = ({onClose}:{
   const lorem = "Lorem ipsum dolor sit amet consectetur, adipisicing elit.";
   const tempComment = `${lorem} ${lorem}`;
 
+  const totalRating = 5;
+  const summedRating = tempDupData.reduce(
+    (prev, { selectedCount }) => prev + selectedCount,
+    0
+  );
+  const tempRating = summedRating / tempDupData.length;
+  const pieChartDeg = (tempRating / totalRating) * 360;
+  console.log({ pieChartDeg });
+
   return (
     <div className="row-details-modal-wrapper">
       <div className="row-details-heading">{headingTitle}</div>
 
       <div className="row-details-body">
-        <div className="row-details-body__pie-chart">73%</div>
+        <div className="row-details-body__pie-chart">
+          <div
+            className="pie"
+            style={{
+              background: `repeating-conic-gradient(from 0deg, #f29d10 0deg ${pieChartDeg}deg, #817b76 ${pieChartDeg}deg 360deg)`,
+            }}
+          ></div>
+          <div className="num">
+            <p>{tempRating.toFixed(1)}</p>
+          </div>
+        </div>
         <div className="row-details-body__ratings">
           <div className="all-ratings">
             {tempDupData.map((data, i) => {
