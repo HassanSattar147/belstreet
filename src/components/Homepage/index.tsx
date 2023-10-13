89f42823d176f518264efa8a5a0e8f449cce3475
nauroz <alinauroze@hotmail.com>
2023-10-13 22:59:26 +0500

    save changes

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -5,6 +5,7 @@ import UnderLine from "../../../public/assets/common/Under line.png";
 import searchPageIcon from "../../../public/assets/common/search-page-icon.svg";
 import evaluatePageIcon from "../../../public/assets/common/evaluate-page-icon.svg";
 import { Link } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
 
 const Homepage = () => {
   return (
@@ -14,36 +15,51 @@ const Homepage = () => {
       <div className="homepage-content">
         <div className="homepage-content-header">
           <h2>
-            Belstreet evaluates the quality of life in all the{" "}
-            <span className="special-text">
-              streets of Brussels
-              <img src={UnderLine} alt="" />
-            </span>
-            .
+          <FormattedMessage
+            id="app.intro"
+            defaultMessage="Belstreet evaluates the quality of life in all the {streets} of Brussels."
+            values={{
+              streets: (
+                <span className="special-text">
+                  streets of Brussels
+                  <img src={UnderLine} alt="" />
+                </span>
+              ),
+            }}
+          />
           </h2>
           <p>
-            Are you moving and want to be 100% sure that there will be no
-            surprises? A noisy or poorly communicated street?
+          <FormattedMessage defaultMessage="Are you moving and want to be 100% sure that there will be no
+            surprises? A noisy or poorly communicated street?" id="hpg.l1" />
           </p>
-          <p>BelStreet is at your side to help you make the right choice.</p>
           <p>
+          <FormattedMessage defaultMessage="
+            BelStreet is at your side to help you make the right choice." id="hpg.l2" />
+          </p>
+          <p>
+          <FormattedMessage defaultMessage="
             Evaluate your street with a few questions or consult our database to
-            consult the evaluation of all the streets in Brussels!
+            consult the evaluation of all the streets in Brussels!" id="hpg.l3" />
           </p>
         </div>
         <div className="page-nav-cards">
           <Link to={"/evaluation-page"} className="page-card">
             <img src={evaluatePageIcon} alt="" />
             <p>
-              Evaluate <br />
-              your street
+            <FormattedMessage
+              id="evaluate.street"
+              defaultMessage="Evaluate your street"
+            />
             </p>
           </Link>
           <Link to={"/search-page"} className="page-card">
             <img src={searchPageIcon} alt="" />
             <p>
-              Search <br />
-              the database
+            <FormattedMessage
+              id="search.database"
+              defaultMessage="Search the database"
+              values={{ lineBreak: <br /> }}
+            />
             </p>
           </Link>
         </div>
