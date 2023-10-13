c52fff47fcd09d9e3f98b12f578de1b677d0d843
nauroz <alinauroze@hotmail.com>
2023-10-13 17:30:04 +0500

    translations

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,25 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import en from "../locale/en.json";
+import fr from "../locale/fr.json";
+import Cookies from "js-cookie";
+
+const messages = {
+  en,
+  fr
+}
+
+const locale = Cookies.get("locale") as "en" || "en";
+const selectedMessages = messages[locale];
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <IntlProvider locale={locale} messages={selectedMessages}>
+        <App />
+      </IntlProvider>
       <Toaster />
     </BrowserRouter>
   </React.StrictMode>
