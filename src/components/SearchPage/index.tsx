0d7c8a39d2d4b49a1578e862bb8174fc396028b6
unknown <hassansattar147@gmail.com>
2023-10-13 14:16:43 +0500

    incorporated Autofill API

--- src/components/SearchPage/index.tsx
+++ src/components/SearchPage/index.tsx
@@ -101,27 +101,7 @@ const index = () => {
       .catch((error) => console.log("error", error));
   };
 
-  const fetchAutofillData = async () => {
-    const formData = new FormData();
-    formData.append("action", "submit");
-    formData.append(
-      "token",
-      "03AFcWeA4itAVmIMsZdzrU1Bk4Jd5fRcJk65yom3il939jzjP7NXr0x-t9Y7EezGtuUGZvk5cc7XGuRym18tF9HaB7TkqlcnG-CIt9HPpFIGZb0sckTj-BlQvWcerzgkPoeXPOlcGCv-6aVEtaEFcUtUcoBdZdIsnSqVmvAvKAqs0UzJoWI_uS0HH8Erp72xJ5pFDglGZPAmf8Imkn8u53AgTQYV56g47aTCpeQlVy_MqIH-TLFVROcmFihlocbAXPddbyqsd7gZp2BiqbweiD_7pnwgrmhaLch_shOUrVl7X7jT1kz3tZdseghEmH9tmco7pMzxKyH_V-d-sPX5AVqnG6TcFW0U1m7bZS_v0RmKh_fZfzEMXaGahdY0JsMSL2Abk0kqKEVLbMRdkEBYnQQBv4iiPGSDBi9kJPvjvSjXJhhUvV4VvcWDGUQPwCU0mbPWTXj-FwqxS62ga_BWHA0v0SJlrfVEcYv44GQJ70rInuKwdoLaZuvsfyiDCBtTcP7tfBEAXMGXW-lh1P1LW8K1mRLxDIjIKhu6-ZH2qtNGw3HeFFmC-JWyPUETHZkDXAs2rc29o3oJ1qM1jutQKtR-eWUTz6sbmrRaaNDt_shNvxaylSj6adkAA"
-    );
-    formData.append("commune", "1050"); //should change depending on the value selected by user. (Municiplity dropdown)
-    formData.append("rue", "rue"); // Search parameter
-    // You can add more form fields here as needed.
 
-    const res = await fetch("http://localhost:3030/Autofill.php", {
-      body: formData,
-      method: "POST",
-      mode: "cors",
-    });
-
-    const data = await res.json();
-
-    console.log("{{Autofill data}}", data);
-  };
 
   const fetchComments = async () => {
     const res = await fetch("http://localhost:3030/Modal_comments.php", {
@@ -138,7 +118,6 @@ const index = () => {
   useEffect(() => {
     fetchData("", "");
     fetchComments();
-    fetchAutofillData();
   }, [pageNo, length]);
 
   useEffect(() => {
