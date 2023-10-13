adb3f9ccebda000879d4953128e43c000072c467
nauroz <alinauroze@hotmail.com>
2023-10-13 23:02:17 +0500

    terms and services

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import EvaluationPage from "./components/EvaluationPage";
 import SearchPage from "./components/SearchPage";
 import Homepage from "./components/Homepage";
 import { Routes, Route } from "react-router-dom";
+import Terms from "./components/Terms";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Route path="/" element={<Homepage />} />
       <Route path="/search-page" element={<SearchPage />} />
       <Route path="/evaluation-page" element={<EvaluationPage />} />
+      <Route path="/terms" element={<Terms />} />
     </Routes>
   );
 }
